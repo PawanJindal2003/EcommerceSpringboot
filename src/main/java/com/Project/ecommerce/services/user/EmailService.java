87b055fb4973d81142527def12ba939d1b13a20f
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-02 16:19:13 +0530

    Added : Registration and verification email sending

---
+++ src/main/java/com/Project/ecommerce/services/user/EmailService.java
@@ -0,0 +1,52 @@
+package com.Project.ecommerce.services.user;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.stereotype.Service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender emailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendActivationEmail(String toEmail, String activationToken) throws MessagingException {
+        String activationUrl = generateActivationUrl(activationToken);
+
+        // Create email content
+        String subject = "Activate Your Account";
+        String body = "<p>Welcome! To activate your account, please click the link below:</p>"
+                + "<p><a href='" + activationUrl + "'>Activate Account</a></p>";
+
+        // Send email
+        sendEmail(toEmail, subject, body);
+    }
+
+    private void sendEmail(String toEmail, String subject, String body) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom(fromEmail);
+        helper.setTo(toEmail);
+        helper.setSubject(subject);
+        helper.setText(body, true); // Set the body as HTML
+
+        emailSender.send(message);
+    }
+
+    private String generateActivationUrl(String token) {
+        String baseUrl = "http://localhost:8080/api/auth/customer/activate";
+        return baseUrl + "?token=" + token;
+    }
+}
