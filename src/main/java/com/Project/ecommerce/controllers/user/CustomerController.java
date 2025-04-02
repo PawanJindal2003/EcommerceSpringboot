94048e1e03e60d863c0d27413dfeb034181fe7d7
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-02 16:09:15 +0530

    Added : /register, /activate, /resend-activation-link

---
+++ src/main/java/com/Project/ecommerce/controllers/user/CustomerController.java
@@ -0,0 +1,44 @@
+package com.Project.ecommerce.controllers.user;
+
+import com.Project.ecommerce.co.CustomerCO;
+import com.Project.ecommerce.entities.user.Customer;
+import com.Project.ecommerce.repositories.user.UserRepository;
+import com.Project.ecommerce.services.user.CustomerService;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/auth/customer")
+@RestController
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+    @Autowired
+    private UserRepository userRepository;
+
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@Valid @RequestBody CustomerCO customerCO) throws MessagingException {
+        customerService.registerCustomer(customerCO);
+
+        return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
+    }
+    @PutMapping("/activate")
+    public ResponseEntity<String> activateCustomer(@RequestParam("token") String token) throws MessagingException {
+        String response = customerService.activateCustomer(token);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+    @PostMapping("/resend-activation-link")
+    public ResponseEntity<String> resendActivationEmail(@RequestParam String email) throws MessagingException {
+        String response = customerService.resendActivationEmail(email);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
