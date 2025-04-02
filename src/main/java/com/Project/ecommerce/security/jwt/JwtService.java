2768ee85c3216b4614969c5d7f5f913909d126f1
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-02 16:17:23 +0530

    Added : Setting up of jwt filter and service

---
+++ src/main/java/com/Project/ecommerce/security/jwt/JwtService.java
@@ -0,0 +1,88 @@
+package com.Project.ecommerce.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtService {
+
+    // Secret Key for signing the JWT. It should be kept private.
+    private static final String SECRET = "TmV3U2VjcmV0S2V5Rm9ySldUU2lnbmluZ1B1cnBvc2VzMTIzNDU2Nzgjbhgtyret";
+
+
+    // Generates a JWT token for the given email.
+    public String generateToken(String email) {
+        // Prepare claims for the token
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 3))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    // Creates a signing key from the base64 encoded secret.
+    //returns a Key object for signing the JWT.
+    private Key getSignKey() {
+        // Decode the base64 encoded secret key and return a Key object
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractEmail(String token) {
+        // Extract and return the subject claim from the token
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        // Extract and return the expiration claim from the token
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimResolver) {
+        // Extract the specified claim using the provided function
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    //Extracts all claims from the JWT token.
+    //return-> Claims object containing all claims.
+    private Claims extractAllClaims(String token) {
+        // Parse and return all claims from the token
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build().parseClaimsJws(token).getBody();
+    }
+
+    //Checks if the JWT token is expired.
+    //return-> True if the token is expired, false otherwise.
+    public Boolean isTokenExpired(String token) {
+        // Check if the token's expiration time is before the current time
+        return extractExpiration(token).before(new Date());
+    }
+
+    //Validates the JWT token against the UserDetails.
+    //return-> True if the token is valid, false otherwise.
+
+    public Boolean isTokenValid(String token, String email) {
+        // Extract email from token
+        final String extractedEmail = extractEmail(token);
+
+        // Ensure email matches and token is not expired
+        return (extractedEmail != null && extractedEmail.equals(email) && !isTokenExpired(token));
+    }
+
+}
