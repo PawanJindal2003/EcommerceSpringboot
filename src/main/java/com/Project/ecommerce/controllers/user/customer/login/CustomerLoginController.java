694ae987ce3fd0317b6b93f8db7c71a034dd23f4
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-06 19:43:09 +0530

    Updated : moved service layer logic

--- src/main/java/com/Project/ecommerce/controllers/user/customer/login/CustomerLoginController.java
+++ src/main/java/com/Project/ecommerce/controllers/user/customer/login/CustomerLoginController.java
@@ -11,8 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,61 +27,17 @@ public class CustomerLoginController {
     private CustomerLoginService customerLoginService;
 
     @Autowired
-    public CustomerLoginController(AuthenticationManager authenticationManager, JwtService jwtService, RedisTokenService redisService, CustomerLoginService customerLoginService){
-        this.authenticationManager = authenticationManager;
-        this.jwtService = jwtService;
-        this.redisService = redisService;
+    public CustomerLoginController(CustomerLoginService customerLoginService) {
         this.customerLoginService = customerLoginService;
-
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginCustomer(@Valid @RequestBody CustomerCO customerCO,  HttpServletResponse response){
-        try{
-            customerLoginService.validateLogin(customerCO.getEmail(), customerCO.getPassword());
-            Authentication auth = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(customerCO.getEmail(), customerCO.getPassword())
-            );
-
-            String token = jwtService.generateAccessToken(customerCO.getEmail());
-
-            Cookie cookie = new Cookie("loginToken", token);
-            cookie.setHttpOnly(true);
-            cookie.setSecure(true);//for https
-            cookie.setPath("/");
-            cookie.setMaxAge(60 * 15);
-            response.addCookie(cookie);
-
-            return new ResponseEntity<>("Login successful", HttpStatus.OK);
-        }
-        catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
-        }
+    public ResponseEntity<String> loginCustomer(@Valid @RequestBody CustomerCO customerCO, HttpServletResponse response) {
+        return customerLoginService.loginCustomer(customerCO, response);
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<?> logoutCustomer(HttpServletRequest request, HttpServletResponse response){
-        String token = null;
-        if(request.getCookies() != null){
-            for(Cookie cookie : request.getCookies()){
-                if("loginToken".equals(cookie.getName())){
-                    token = cookie.getValue();
-                    break;
-                }
-            }
-        }
-
-        if(token != null){
-            redisService.revokeToken(token);
-        }
-
-        Cookie cookie = new Cookie("loginToken", null);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(0); // Expire it immediately
-        response.addCookie(cookie);
-
-        return ResponseEntity.ok("Logged out");
+    public ResponseEntity<?> logoutCustomer(HttpServletRequest request, HttpServletResponse response) {
+        return customerLoginService.logoutCustomer(request, response);
     }
 }
