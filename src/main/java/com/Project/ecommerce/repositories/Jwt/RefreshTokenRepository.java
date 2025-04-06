c0fbcf1fb828c6f46f35187793f040218be3f2bc
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-07 04:27:50 +0530

    Added : RefreshToken repository

---
+++ src/main/java/com/Project/ecommerce/repositories/Jwt/RefreshTokenRepository.java
@@ -0,0 +1,18 @@
+package com.Project.ecommerce.repositories.Jwt;
+
+import com.Project.ecommerce.entities.jwt.RefreshToken;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByEmail(String email);
+    @Modifying
+    @Transactional
+    void deleteByEmail(String email);
+}
