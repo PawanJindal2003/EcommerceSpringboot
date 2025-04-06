ba94bc59647b78cf0cf2554f84dcb65baa5a5a62
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-06 08:47:09 +0530

    Added : Seller Repository with findByEmail finder method

--- src/main/java/com/Project/ecommerce/repositories/user/SellerRepository.java
+++ src/main/java/com/Project/ecommerce/repositories/user/SellerRepository.java
@@ -1,9 +1,12 @@
 package com.Project.ecommerce.repositories.user;
 
 import com.Project.ecommerce.entities.user.Seller;
+import com.Project.ecommerce.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SellerRepository extends JpaRepository<Seller, UUID> {
+    Optional<User> findByEmail(String email);
 }
