3d8e85914bbf38240b9beaf1ae17e8f31d829c8b
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-06 19:46:22 +0530

    Updated : added inactiveUser handler and removed credential Expired handler

--- src/main/java/com/Project/ecommerce/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/Project/ecommerce/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,6 @@
 package com.Project.ecommerce.exceptions;
 
-import com.Project.ecommerce.exceptions.customExceptions.ConfirmPasswordMismatchException;
-import com.Project.ecommerce.exceptions.customExceptions.DuplicateCompanyException;
-import com.Project.ecommerce.exceptions.customExceptions.EmailAlreadyExistsException;
-import com.Project.ecommerce.exceptions.customExceptions.UserNotFoundException;
+import com.Project.ecommerce.exceptions.customExceptions.*;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureException;
@@ -11,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -71,9 +69,15 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new ErrorResponse(404, "Either email or password incorrect", errorMessages), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(CredentialsExpiredException.class)
-    public ResponseEntity<ErrorResponse> handleCredentialsExpiredException(CredentialsExpiredException ex){
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<ErrorResponse> handleLockedException(LockedException ex){
         List<String> errorMessages = List.of(ex.getMessage());
-        return new ResponseEntity<>(new ErrorResponse(401, "Session expired, please re-login", errorMessages), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(new ErrorResponse(403, "Account is locked due to multiple failed attempts", errorMessages), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(InactiveUserException.class)
+    public ResponseEntity<ErrorResponse> handleInactiveUserException(InactiveUserException ex){
+        List<String> errorMessages = List.of(ex.getMessage());
+        return new ResponseEntity<>(new ErrorResponse(404, "Inactive account", errorMessages), HttpStatus.BAD_REQUEST);
     }
 }
