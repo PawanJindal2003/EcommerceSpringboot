b117f4b12a326849396529a4ceab7ce771ba1ca1
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-06 09:22:31 +0530

    Renamed : ConfirmPasswordMismatch to ConfirmPasswordMismatchException

--- src/main/java/com/Project/ecommerce/exceptions/customExceptions/ConfirmPasswordMismatchException.java
+++ src/main/java/com/Project/ecommerce/exceptions/customExceptions/ConfirmPasswordMismatchException.java
@@ -1,7 +1,7 @@
 package com.Project.ecommerce.exceptions.customExceptions;
 
-public class ConfirmPasswordMismatch extends RuntimeException{
-    public ConfirmPasswordMismatch(String message){
+public class ConfirmPasswordMismatchException extends RuntimeException{
+    public ConfirmPasswordMismatchException(String message){
         super(message);
     }
 }
