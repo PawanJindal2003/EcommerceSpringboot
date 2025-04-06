d5b77aa795f7243120b40c5521c42ea9ba98cc24
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-07 04:28:26 +0530

    Updated : Added refresh token field

--- src/main/java/com/Project/ecommerce/dto/login/CustomerDTO.java
+++ src/main/java/com/Project/ecommerce/dto/login/CustomerDTO.java
@@ -7,9 +7,11 @@ import lombok.Setter;
 @Getter
 public class CustomerDTO {
     private String accessToken;
+    private String refreshToken;
     private String message;
-    public CustomerDTO(String accessToken, String message){
+    public CustomerDTO(String accessToken, String refreshToken, String message){
         this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
         this.message = message;
     }
 }
