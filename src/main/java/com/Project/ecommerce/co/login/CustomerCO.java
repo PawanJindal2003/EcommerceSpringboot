1275edda40fb6564e45647292f6fc7a6d6d4c1af
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-06 12:08:09 +0530

    Added login COs

--- src/main/java/com/Project/ecommerce/co/login/CustomerCO.java
+++ src/main/java/com/Project/ecommerce/co/login/CustomerCO.java
@@ -1,4 +1,17 @@
 package com.Project.ecommerce.co.login;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class CustomerCO {
+    @Email(message = "This is invalid email format, please enter correct email")
+    String email;
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,15}$",
+            message = "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character")
+    private String password;
 }
--- src/main/java/com/Project/ecommerce/co/login/SellerCO.java
+++ src/main/java/com/Project/ecommerce/co/login/SellerCO.java
@@ -1,4 +1,9 @@
 package com.Project.ecommerce.co.login;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class SellerCO {
 }
