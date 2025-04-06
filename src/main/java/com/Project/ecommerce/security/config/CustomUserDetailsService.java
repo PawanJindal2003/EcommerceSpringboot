bd27e68f0b8a820609f83a33a112cb7dfc2a6669
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-06 17:19:00 +0530

    Updated : accountLocked and activeAccount validations

--- src/main/java/com/Project/ecommerce/security/config/CustomUserDetailsService.java
+++ src/main/java/com/Project/ecommerce/security/config/CustomUserDetailsService.java
@@ -1,8 +1,11 @@
 package com.Project.ecommerce.security.config;
 
+
 import com.Project.ecommerce.entities.user.User;
 import com.Project.ecommerce.repositories.user.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,14 +14,24 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    private final UserRepository userRepository;
+    private UserRepository userRepository;
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Optional<User> user = userRepository.findByEmail(email);
+        if (!user.get().getIsActive()){
+            throw new DisabledException("Account is not active");
+        }
+        if(user.get().getIsLocked()){
+            throw new LockedException("Account is locked, due to multiple incorrect login attempts");
+        }
         return user.map(CustomUserDetails::new)
                 .orElseThrow(() -> new UsernameNotFoundException("UserName not found: " + email));
     }
+
 }
