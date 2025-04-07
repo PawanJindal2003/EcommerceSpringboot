84fbe5df75458111dce64a431f6087caf4a8e2fc
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-07 11:41:47 +0530

    Added : forgetPassword repository

---
+++ src/main/java/com/Project/ecommerce/repositories/Jwt/ForgetPasswordTokenRepository.java
@@ -0,0 +1,18 @@
+package com.Project.ecommerce.repositories.Jwt;
+
+import com.Project.ecommerce.entities.jwt.ForgetPasswordToken;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ForgetPasswordTokenRepository extends JpaRepository<ForgetPasswordToken, UUID> {
+    public Optional<ForgetPasswordToken> findByEmail(String email);
+    @Modifying
+    @Transactional
+    public void deleteByEmail(String email);
+}
