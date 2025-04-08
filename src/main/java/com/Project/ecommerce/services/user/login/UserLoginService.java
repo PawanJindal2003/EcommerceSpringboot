a7727adc8b1941268ad958d821eaab4432ed8713
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-08 14:17:37 +0530

    Updated : removed refreshToken method and corrected the isLocked logic

--- src/main/java/com/Project/ecommerce/services/user/login/UserLoginService.java
+++ src/main/java/com/Project/ecommerce/services/user/login/UserLoginService.java
@@ -2,7 +2,6 @@ package com.Project.ecommerce.services.user.login;
 
 import com.Project.ecommerce.co.login.UserCO;
 import com.Project.ecommerce.dto.login.UserDTO;
-import com.Project.ecommerce.entities.jwt.RefreshToken;
 import com.Project.ecommerce.entities.user.User;
 import com.Project.ecommerce.exceptions.customExceptions.InactiveUserException;
 import com.Project.ecommerce.exceptions.customExceptions.UserNotFoundException;
@@ -22,8 +21,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.Optional;
-
 @Service
 public class UserLoginService {
     private UserRepository userRepository;
@@ -42,85 +39,49 @@ public class UserLoginService {
     }
 
     public ResponseEntity<UserDTO> loginCustomer(@Valid @RequestBody UserCO userCO, HttpServletResponse response) {
-        try {
-            if(!userRepository.findByEmail(userCO.getEmail()).get().getIsActive()){
-                throw new InactiveUserException("Please activate your account");
-            }
-//            customerLoginService.validateLogin(customerCO.getEmail(), customerCO.getPassword());
+        User user = userRepository.findByEmail(userCO.getEmail()).orElseThrow(()->new UserNotFoundException("Email not found"));
+        if (!user.getIsActive()) {
+            throw new InactiveUserException("Please activate your account");
+        }
+
+        try{
             Authentication auth = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(userCO.getEmail(), userCO.getPassword())
             );
-
-            String accessToken = jwtService.generateAccessToken(userCO.getEmail());
-
-            //deleting previous refresh token while logging in
-            jwtService.deleteRefreshToken(userCO.getEmail());
-            String refreshToken = jwtService.generateRefreshToken(userCO.getEmail());
-            //saving the newly generated token
-            jwtService.storeRefreshToken(refreshToken, userCO.getEmail());
-
-            Cookie accessCookie = new Cookie("loginToken", accessToken);
-            accessCookie.setHttpOnly(true);
-            accessCookie.setSecure(true);//for https
-            accessCookie.setPath("/");
-            accessCookie.setMaxAge(60 * 15);
-            response.addCookie(accessCookie);
-
-            Cookie refreshCookie = new Cookie("refreshToken", refreshToken);
-            refreshCookie.setHttpOnly(true);
-            refreshCookie.setSecure(true);//for https
-            refreshCookie.setPath("/");
-            refreshCookie.setMaxAge(60 * 60 * 24);
-            response.addCookie(refreshCookie);
-
-            UserDTO dto = new UserDTO(accessToken, refreshToken,"Login successful");
-            return new ResponseEntity<>(dto, HttpStatus.OK);
-        } catch (BadCredentialsException ex) {
+        }
+        catch (BadCredentialsException e){
             multipleLoginAttempts(userCO.getEmail());
-            return new ResponseEntity<>(new UserDTO(null, null,"Either email or password incorrect"), HttpStatus.NOT_FOUND);
-        }catch (InternalAuthenticationServiceException e) {
-            return new ResponseEntity<>(new UserDTO(null, null,"Ether email or password incorrect"), HttpStatus.NOT_FOUND);
+            throw e;
         }
+
+        String accessToken = jwtService.generateAccessToken(userCO.getEmail());
+
+        //deleting previous refresh token while logging in
+        jwtService.deleteRefreshToken(userCO.getEmail());
+        String refreshToken = jwtService.generateRefreshToken(userCO.getEmail());
+        //saving the newly generated token
+        jwtService.storeRefreshToken(refreshToken, userCO.getEmail());
+        redisService.storeToken(accessToken, 15 * 60 * 1000);
+
+        Cookie accessCookie = new Cookie("accessToken", accessToken);
+        accessCookie.setHttpOnly(true);
+        accessCookie.setSecure(true);//for https
+        accessCookie.setPath("/");
+        accessCookie.setMaxAge(60 * 15);
+        response.addCookie(accessCookie);
+
+        UserDTO dto = new UserDTO(accessToken, "Login successful");
+        user.setInvalidAttemptCount(0);
+        userRepository.save(user);
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
-    public ResponseEntity<UserDTO> refreshAccessToken(HttpServletRequest request, HttpServletResponse response) {
-        String refreshToken = null;
-
-        // Example: Get from cookie
-        if (request.getCookies() != null) {
-            for (Cookie cookie : request.getCookies()) {
-                if ("refreshToken".equals(cookie.getName())) {
-                    refreshToken = cookie.getValue();
-                    break;
-                }
-            }
-        }
-        if (refreshToken == null) {
-            return new ResponseEntity<>(new UserDTO(null, null, "Refresh token missing"), HttpStatus.UNAUTHORIZED);
-        }
-
-        String email = jwtService.extractEmail(refreshToken);
-        if (!jwtService.isTokenValid(refreshToken, email)) {
-            return new ResponseEntity<>(new UserDTO(null, null, "Invalid or expired refresh token"), HttpStatus.UNAUTHORIZED);
-        }
-
-        Optional<RefreshToken> storedToken = refreshTokenRepository.findByEmail(userRepository.findByEmail(email).get().getEmail());
-        if (storedToken.isEmpty() || !storedToken.get().getToken().equals(refreshToken)) {
-            return new ResponseEntity<>(new UserDTO(null, null, "Refresh token mismatch"), HttpStatus.UNAUTHORIZED);
-        }
-
-        String newAccessToken = jwtService.generateAccessToken(email);
-        UserDTO dto = new UserDTO(newAccessToken, refreshToken, "Token refreshed successfully");
-        return ResponseEntity.ok(dto);
-    }
-
-
-    public void multipleLoginAttempts(String email){
-        User user = userRepository.findByEmail(email).orElseThrow(()->new UserNotFoundException("Email not found"));
+    public void multipleLoginAttempts(String email) {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Email not found"));
         int attempts = user.getInvalidAttemptCount() + 1;
         user.setInvalidAttemptCount(attempts);
 
-        if(attempts>=3){
+        if (attempts >= 3) {
             user.setIsLocked(true);
         }
 
@@ -131,7 +92,7 @@ public class UserLoginService {
         String accessToken = null;
         if (request.getCookies() != null) {
             for (Cookie cookie : request.getCookies()) {
-                if ("loginToken".equals(cookie.getName())) {
+                if ("accessToken".equals(cookie.getName())) {
                     accessToken = cookie.getValue();
                     break;
                 }
@@ -148,7 +109,7 @@ public class UserLoginService {
         jwtService.deleteRefreshToken(email);
 
         //clearing the cookie
-        Cookie cookie = new Cookie("loginToken", null);
+        Cookie cookie = new Cookie("accessToken", null);
         cookie.setHttpOnly(true);
         cookie.setSecure(true);
         cookie.setPath("/");
