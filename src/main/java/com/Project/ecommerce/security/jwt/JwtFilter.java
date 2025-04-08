f2a11fe54ffe2cdd7e6f6836e57f460024463cab
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-08 14:36:02 +0530

    Updated : added nonPublicURLs and logic for handling of refresh token

--- src/main/java/com/Project/ecommerce/security/jwt/JwtFilter.java
+++ src/main/java/com/Project/ecommerce/security/jwt/JwtFilter.java
@@ -1,83 +1,164 @@
 package com.Project.ecommerce.security.jwt;
 
+import com.Project.ecommerce.entities.jwt.RefreshToken;
+import com.Project.ecommerce.repositories.Jwt.RefreshTokenRepository;
 import com.Project.ecommerce.security.config.CustomUserDetailsService;
 import com.Project.ecommerce.security.redis.RedisTokenService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
+    private static final List<String> NON_PUBLIC_URLS = List.of("/api/auth/customer/logout");
     private JwtService jwtService;
-    private ApplicationContext applicationContext;
     private RedisTokenService redisTokenService;
+    private CustomUserDetailsService customUserDetailsService;
+    private RefreshTokenRepository refreshTokenRepository;
 
     @Autowired
-    public JwtFilter(JwtService jwtService, ApplicationContext applicationContext, RedisTokenService redisTokenService){
+    public JwtFilter(JwtService jwtService, RedisTokenService redisTokenService, CustomUserDetailsService customUserDetailsService, RefreshTokenRepository refreshTokenRepository) {
         this.jwtService = jwtService;
-        this.applicationContext = applicationContext;
         this.redisTokenService = redisTokenService;
-    }
-
-    // Method to lazily fetch the UserService bean from the ApplicationContext
-    // This is done to avoid Circular Dependency issues
-    private CustomUserDetailsService getUserService() {
-        return applicationContext.getBean(CustomUserDetailsService.class);
+        this.customUserDetailsService = customUserDetailsService;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // Extracting token from the request header
-        String authHeader = request.getHeader("Authorization");
+        String path = request.getRequestURI();
         String token = null;
         String email = null;
-
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            // Extracting the token from the Authorization header
-            token = authHeader.substring(7);
-            // Extracting username from the token
-            email = jwtService.extractEmail(token);
-        }
-
         //retrieving cookie
         if (request.getCookies() != null) {
             for (Cookie cookie : request.getCookies()) {
-                if ("loginToken".equals(cookie.getName())) {
+                if ("accessToken".equals(cookie.getName())) {
                     token = cookie.getValue();
                     break;
                 }
             }
         }
 
-        // If username is extracted and there is no authentication in the current SecurityContext
-        if (email != null && jwtService.isAccessTokenValid(token) && redisTokenService.isTokenValid(token)) {
-            // Loading UserDetails by username extracted from the token
-            UserDetails userDetails = getUserService().loadUserByUsername(email);
+        if (!NON_PUBLIC_URLS.contains(path)) {
+            filterChain.doFilter(request, response);
+            return;
+        } else if (token == null) {
+            sendErrorResponse(response, List.of("Authentication Failed: JWT Missing"), HttpStatus.UNAUTHORIZED);
+            return;
+        }
 
-            // Validating the token with loaded UserDetails
-            if (SecurityContextHolder.getContext().getAuthentication() == null) {
-                // Creating an authentication token using UserDetails
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                // Setting authentication details
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                // Setting the authentication token in the SecurityContext
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+        try {
+            if(SecurityContextHolder.getContext().getAuthentication() == null){
+                email = jwtService.extractEmail(token);
+                if(!redisTokenService.isTokenValid(token)){
+                    sendErrorResponse(response, List.of("Jwt validation failed: Token invalid"), HttpStatus.UNAUTHORIZED);
+                    return;
+                }
+                if(email!=null){
+                    UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+
+                    if(jwtService.isTokenValid(token, email)){
+                        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authToken);
+                    }
+                }
+            }
+        }
+        catch (ExpiredJwtException e) {
+            handleRefreshTokenIfExists(e, request, response, filterChain);
+        }
+        catch (SignatureException e){
+            sendErrorResponse(response, List.of("JWT validation failed : Invalid signature"),HttpStatus.UNAUTHORIZED);
+            return;
+        }
+        catch (MalformedJwtException e){
+            sendErrorResponse(response, List.of("JWT validation failed : Missing part in JWT"), HttpStatus.UNAUTHORIZED);
+            return;
+        }
+        catch (Exception e) {
+            sendErrorResponse(response, List.of("Unexpected error occurred"), HttpStatus.INTERNAL_SERVER_ERROR);
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void sendErrorResponse(HttpServletResponse response, List<String> errorMessages, HttpStatus httpStatus) throws IOException{
+        response.setStatus(httpStatus.value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String timeStamp = LocalDateTime.now().toString();
+
+        String json = """
+                {
+                    "errorMessage" : %s,
+                    "errorOccurred": %s,
+                    "statusCode: %s"
+                }
+                """.formatted(toJsonArray(errorMessages), timeStamp, httpStatus.name());
+
+        response.getWriter().write(json);
+    }
+    private String toJsonArray(List<String> list) {
+        return list.stream()
+                .map(msg -> "\"" + msg.replace("\"", "\\\"") + "\"")
+                .collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    private void handleRefreshTokenIfExists(ExpiredJwtException e, HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException {
+        String email = e.getClaims().getSubject();
+        if (email != null) {
+            RefreshToken refreshToken = refreshTokenRepository.findByEmail(email).orElseThrow(()->new RuntimeException("Refresh Token not found"));
+
+            try{
+                if (refreshToken != null && jwtService.isTokenValid(refreshToken.getToken(), refreshToken.getEmail())) {
+                    String newAccessToken = jwtService.generateCustomToken(email, 1000L * 60 * 15);
+
+                    //store in redis
+                    redisTokenService.storeToken(newAccessToken, 1000 * 60 * 15);
+
+                    //send in cookie
+                    Cookie newCookie = new Cookie("accessToken", newAccessToken);
+                    newCookie.setMaxAge(1000 * 60 * 15);
+                    newCookie.setPath("/");
+                    newCookie.setHttpOnly(true);
+                    response.addCookie(newCookie);
+
+                    UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities()
+                    );
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+
+                    filterChain.doFilter(request, response);
+                }
+            }
+            catch (ExpiredJwtException ex) {
+                sendErrorResponse(response, List.of("Session expired. Please login again."), HttpStatus.UNAUTHORIZED);
+            } catch (ServletException ex) {
+                throw new RuntimeException(ex);
             }
         }
 
-        // Proceeding with the filter chain
-        filterChain.doFilter(request, response);
     }
 }
+
