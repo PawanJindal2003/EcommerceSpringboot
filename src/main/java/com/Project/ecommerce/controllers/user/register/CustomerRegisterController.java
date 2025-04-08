f11a4a64677f62266432504906fa092cc259ffdd
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-08 17:01:13 +0530

    Updated : added validation on resendActivationEmail

--- src/main/java/com/Project/ecommerce/controllers/user/register/CustomerRegisterController.java
+++ src/main/java/com/Project/ecommerce/controllers/user/register/CustomerRegisterController.java
@@ -4,6 +4,9 @@ import com.Project.ecommerce.co.registration.CustomerCO;
 import com.Project.ecommerce.services.user.register.customer.CustomerRegisterService;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +18,7 @@ public class CustomerRegisterController {
     private CustomerRegisterService customerRegisterService;
 
     @Autowired
-    public CustomerRegisterController(CustomerRegisterService customerRegisterService){
+    public CustomerRegisterController(CustomerRegisterService customerRegisterService) {
         this.customerRegisterService = customerRegisterService;
     }
 
@@ -24,12 +27,17 @@ public class CustomerRegisterController {
         String response = customerRegisterService.registerCustomer(customerCO);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
     @PutMapping("/activate")
     public ResponseEntity<String> activateCustomer(@RequestParam("token") String token) throws MessagingException {
         return customerRegisterService.activateCustomer(token);
     }
+
     @PostMapping("/resend-activation-link")
-    public ResponseEntity<String> resendActivationEmail(@RequestParam String email) throws MessagingException {
+    public ResponseEntity<String> resendActivationEmail(@RequestParam
+                                                            @NotBlank(message = "Email is required")
+                                                            @Email(message = "Invalid email format")
+                                                            @Size(min = 6, max = 256, message = "Email must be between 6 and 256 characters") String email) throws MessagingException {
         String response = customerRegisterService.resendActivationEmail(email);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
