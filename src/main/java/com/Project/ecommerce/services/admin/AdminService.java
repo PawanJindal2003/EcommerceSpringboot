c20d276d9bbbc6c0591a0714abf3778ce7ad6659
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-09 17:54:09 +0530

    Updated : added get-all-sellers service logic

--- src/main/java/com/Project/ecommerce/services/admin/AdminService.java
+++ src/main/java/com/Project/ecommerce/services/admin/AdminService.java
@@ -1,8 +1,10 @@
 package com.Project.ecommerce.services.admin;
 
 import com.Project.ecommerce.dto.admin.GetAllCustomersDTO;
+import com.Project.ecommerce.dto.admin.GetAllSellersDTO;
+import com.Project.ecommerce.entities.address.Address;
 import com.Project.ecommerce.repositories.user.CustomerRepository;
-import com.Project.ecommerce.repositories.user.UserRepository;
+import com.Project.ecommerce.repositories.user.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,12 @@ import java.util.stream.Collectors;
 @Service
 public class AdminService {
     private CustomerRepository customerRepository;
+    private SellerRepository sellerRepository;
 
     @Autowired
-    public AdminService(CustomerRepository customerRepository, UserRepository userRepository) {
+    public AdminService(CustomerRepository customerRepository, SellerRepository sellerRepository) {
         this.customerRepository = customerRepository;
+        this.sellerRepository = sellerRepository;
     }
 
     public List<GetAllCustomersDTO> getAllCustomers(Pageable pageable) {
@@ -42,6 +46,35 @@ public class AdminService {
             customerDTO.setEmail(customer.getEmail());
             customerDTO.setIsActive(customer.getIsActive());
             return customerDTO;
+        }).collect(Collectors.toList());
+    }
+
+    public List<GetAllSellersDTO> getAllSellers(Pageable pageable) {
+        return sellerRepository.findAll(pageable).stream().map(seller -> {
+            GetAllSellersDTO sellersDTO = new GetAllSellersDTO();
+            sellersDTO.setId(seller.getID());
+            sellersDTO.setFirstName(seller.getFirstName());
+            sellersDTO.setMiddleName(seller.getMiddleName());
+            sellersDTO.setLastName(seller.getLastName());
+            sellersDTO.setEmail(seller.getEmail());
+            sellersDTO.setIsActive(seller.getIsActive());
+            return sellersDTO;
+        }).collect(Collectors.toList());
+    }
+
+    public List<GetAllSellersDTO> getAllSellersByEmail(String email, Pageable pageable) {
+        return sellerRepository.findAllByEmail(email, pageable).stream().map(seller -> {
+            GetAllSellersDTO sellersDTO = new GetAllSellersDTO();
+            sellersDTO.setId(seller.getID());
+            sellersDTO.setFirstName(seller.getFirstName());
+            sellersDTO.setMiddleName(seller.getMiddleName());
+            sellersDTO.setLastName(seller.getLastName());
+            sellersDTO.setEmail(seller.getEmail());
+            sellersDTO.setIsActive(seller.getIsActive());
+            sellersDTO.setCompanyName(seller.getCompanyName());
+            sellersDTO.setCompanyAddress(seller.getAddresses().get(0));
+            sellersDTO.setCompanyContact(seller.getCompanyContact());
+            return sellersDTO;
         }).collect(Collectors.toList());
     }
 }
