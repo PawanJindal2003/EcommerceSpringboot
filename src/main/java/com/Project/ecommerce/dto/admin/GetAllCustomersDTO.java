7a7dbc7f0c3f7beef3c13d7424d22b25b04afc74
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-09 15:54:22 +0530

    Added : get all user dto

---
+++ src/main/java/com/Project/ecommerce/dto/admin/GetAllCustomersDTO.java
@@ -0,0 +1,19 @@
+package com.Project.ecommerce.dto.admin;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class GetAllCustomersDTO {
+    private UUID id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private Boolean isActive;
+}
