0589ca7626f1a55ca16c38af1867cf1ff5fc0428
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-09 22:08:03 +0530

    Added : CO for getting userID as input for activation/deactivation feature

---
+++ src/main/java/com/Project/ecommerce/co/admin/GetIdCO.java
@@ -0,0 +1,12 @@
+package com.Project.ecommerce.co.admin;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class GetIdCO {
+    private UUID id;
+}
