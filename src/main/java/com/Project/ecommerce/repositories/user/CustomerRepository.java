7c5c2091d73ab820cc1032c9ded69cec0a22b325
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-09 15:48:09 +0530

    Updated : added findByEmail finder method

--- src/main/java/com/Project/ecommerce/repositories/user/CustomerRepository.java
+++ src/main/java/com/Project/ecommerce/repositories/user/CustomerRepository.java
@@ -2,7 +2,11 @@ package com.Project.ecommerce.repositories.user;
 
 import com.Project.ecommerce.entities.user.Customer;
 import com.Project.ecommerce.entities.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +14,7 @@ import java.util.UUID;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    @Query("select c from Customer c WHERE (:email is null or lower(c.email) like lower(concat('%', :email, '%')))")
+    Page<Customer> findAllByEmail(@Param("email") String email, Pageable pageable);
     Optional<User> findByEmail(String email);
 }
