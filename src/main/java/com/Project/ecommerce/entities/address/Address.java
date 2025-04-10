d160bc90e7e9074e9e7e789176c7cdd53eb20655
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-10 13:50:53 +0530

    Updated : added validators

--- src/main/java/com/Project/ecommerce/entities/address/Address.java
+++ src/main/java/com/Project/ecommerce/entities/address/Address.java
@@ -1,6 +1,7 @@
 package com.Project.ecommerce.entities.address;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.util.UUID;
@@ -15,10 +16,22 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @NotBlank(message = "City cannot be blank")
     private String city;
+
+
+    @NotBlank(message = "State cannot be blank")
     private String state;
+
+
+    @NotBlank(message = "Country cannot be blank")
     private String country;
+
+    @NotBlank(message = "Address cannot be blank")
     private String addressLine;
+
+    @NotBlank(message = "Zipcode cannot be blank")
     private String zipCode;
+
     private String label;
 }
