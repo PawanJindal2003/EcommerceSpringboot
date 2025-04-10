45e6889b3da9f8a0f529dad4e5606c8a9e666271
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-10 16:13:52 +0530

    Updated : added update-profile, update-password, add-address, delete-address, update-address

--- src/main/java/com/Project/ecommerce/services/cutomer/CustomerService.java
+++ src/main/java/com/Project/ecommerce/services/cutomer/CustomerService.java
@@ -1,16 +1,21 @@
 package com.Project.ecommerce.services.cutomer;
 
+import com.Project.ecommerce.co.customer.UpdateProfileCO;
+import com.Project.ecommerce.co.user.UpdateAddressCO;
+import com.Project.ecommerce.co.user.UpdatePasswordCO;
 import com.Project.ecommerce.dto.customer.ViewAddressDTO;
 import com.Project.ecommerce.dto.customer.ViewProfileDTO;
 import com.Project.ecommerce.entities.address.Address;
 import com.Project.ecommerce.entities.user.Customer;
+import com.Project.ecommerce.exceptions.customExceptions.ConfirmPasswordMismatchException;
 import com.Project.ecommerce.exceptions.customExceptions.UserNotFoundException;
+import com.Project.ecommerce.repositories.user.AddressRepository;
 import com.Project.ecommerce.repositories.user.CustomerRepository;
-import com.Project.ecommerce.security.jwt.JwtFilter;
 import com.Project.ecommerce.security.jwt.JwtService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -19,23 +24,28 @@ import java.util.*;
 public class CustomerService {
     private JwtService jwtService;
     private CustomerRepository customerRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private AddressRepository addressRepository;
+
     @Autowired
-    public CustomerService(JwtService jwtService, CustomerRepository customerRepository){
+    public CustomerService(JwtService jwtService, CustomerRepository customerRepository, BCryptPasswordEncoder bCryptPasswordEncoder, AddressRepository addressRepository) {
         this.jwtService = jwtService;
         this.customerRepository = customerRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.addressRepository = addressRepository;
     }
 
-    public ViewProfileDTO viewProfile(HttpServletRequest request){
+    public ViewProfileDTO viewProfile(HttpServletRequest request) {
         String accessToken = null;
-        if(request.getCookies()!=null){
-            for(Cookie cookie:request.getCookies()){
-                if(cookie.getName().equals("accessToken")){
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("accessToken")) {
                     accessToken = cookie.getValue();
                 }
             }
         }
         String email = jwtService.extractEmail(accessToken);
-        Customer customer = customerRepository.findByEmail(email).orElseThrow(()->new UserNotFoundException("Email not found"));
+        Customer customer = customerRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Email not found"));
 
         ViewProfileDTO viewProfileDTO = new ViewProfileDTO();
 
@@ -50,23 +60,23 @@ public class CustomerService {
         return viewProfileDTO;
     }
 
-    public List<ViewAddressDTO> getAllAddresses(HttpServletRequest request){
+    public List<ViewAddressDTO> getAllAddresses(HttpServletRequest request) {
         String accessToken = null;
 
-        if(request.getCookies()!=null){
-            for(Cookie cookie:request.getCookies()){
-                if(cookie.getName().equals("accessToken")){
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("accessToken")) {
                     accessToken = cookie.getValue();
                 }
             }
         }
 
         String email = jwtService.extractEmail(accessToken);
-        Customer customer = customerRepository.findByEmail(email).orElseThrow(()->new UserNotFoundException("Email not found"));
+        Customer customer = customerRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Email not found"));
 
         List<ViewAddressDTO> addressesDTO = new ArrayList<>();
 
-        for(Address address : customer.getAddresses()){
+        for (Address address : customer.getAddresses()) {
             ViewAddressDTO addressDTO = new ViewAddressDTO();
 
             addressDTO.setAddressLine(address.getAddressLine());
@@ -75,10 +85,155 @@ public class CustomerService {
             addressDTO.setCountry(address.getCountry());
             addressDTO.setZipCode(address.getZipCode());
             addressDTO.setLabel(address.getLabel());
-            
+
             addressesDTO.add(addressDTO);
         }
-        
+
         return addressesDTO;
+    }
+
+    public String updateProfile(HttpServletRequest request, UpdateProfileCO updateProfileCO) {
+        String accessToken = null;
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("accessToken")) {
+                    accessToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        String email = jwtService.extractEmail(accessToken);
+        Customer customer = customerRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Customer not found"));
+
+        Optional.ofNullable(updateProfileCO.getFirstName()).ifPresent(customer::setFirstName);
+        Optional.ofNullable(updateProfileCO.getMiddleName()).ifPresent(customer::setMiddleName);
+        Optional.ofNullable(updateProfileCO.getLastName()).ifPresent(customer::setLastName);
+        Optional.ofNullable(updateProfileCO.getCustomerContact()).ifPresent(customer::setCustomerContact);
+
+        customerRepository.save(customer);
+        return "Profile updated successfully.";
+    }
+
+    public String updatePassword(HttpServletRequest request, UpdatePasswordCO updatePasswordCO) {
+        // check if password and confirm password are not different
+        if (!updatePasswordCO.getPassword().equals(updatePasswordCO.getConfirmPassword())) {
+            throw new ConfirmPasswordMismatchException("Password-Confirm password mismatch");
+        }
+        String accessToken = null;
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("accessToken")) {
+                    accessToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        String email = jwtService.extractEmail(accessToken);
+        Customer customer = customerRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Customer not found"));
+
+        //updating password
+        customer.setPassword(bCryptPasswordEncoder.encode(updatePasswordCO.getPassword()));
+
+        customerRepository.save(customer);
+
+        return "Your password has been successfully updated";
+    }
+
+    public String addAddress(HttpServletRequest request, Address enteredNewAddress) {
+        String accessToken = null;
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("accessToken")) {
+                    accessToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        String email = jwtService.extractEmail(accessToken);
+        Customer customer = customerRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Customer not found"));
+
+        Address newAddress = new Address();
+        newAddress.setAddressLine(enteredNewAddress.getAddressLine());
+        newAddress.setCity(enteredNewAddress.getCity());
+        newAddress.setState(enteredNewAddress.getState());
+        newAddress.setCountry(enteredNewAddress.getCountry());
+        newAddress.setZipCode(enteredNewAddress.getZipCode());
+        newAddress.setLabel(enteredNewAddress.getLabel());
+
+        List<Address> addresses = customer.getAddresses();
+        addresses.add(newAddress);
+
+        customer.setAddresses(addresses);
+
+        customerRepository.save(customer);
+
+        return "Address added successfully";
+    }
+
+    public String deleteAddress(HttpServletRequest request, UUID id) {
+        String accessToken = null;
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("accessToken")) {
+                    accessToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        String email = jwtService.extractEmail(accessToken);
+        Customer customer = customerRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Customer not found"));
+
+        List<Address> addresses = customer.getAddresses();
+
+        int i = 0;
+        for (Address address : addresses) {
+            if (address.getId() == id) {
+                addressRepository.deleteById(id);
+                return "Address deleted successfully";
+            }
+            i += 1;
+        }
+
+        if (i >= addresses.size()) {
+            return "Address not found";
+        }
+        return "Address deleted successfully";
+    }
+
+    public String updateAddress(HttpServletRequest request, UUID id, UpdateAddressCO updateAddressCO) {
+        String accessToken = null;
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("accessToken")) {
+                    accessToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        String email = jwtService.extractEmail(accessToken);
+        Customer customer = customerRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("Customer not found"));
+
+        List<Address> addresses = customer.getAddresses();
+
+
+        int i = 0;
+        for (Address address : addresses) {
+            if (address.getId() == id) {
+                Optional.ofNullable(updateAddressCO.getAddressLine()).ifPresent(address::setAddressLine);
+                Optional.ofNullable(updateAddressCO.getCity()).ifPresent(address::setCity);
+                Optional.ofNullable(updateAddressCO.getState()).ifPresent(address::setState);
+                Optional.ofNullable(updateAddressCO.getCountry()).ifPresent(address::setCountry);
+                Optional.ofNullable(updateAddressCO.getZipCode()).ifPresent(address::setZipCode);
+            }
+            i += 1;
+        }
+
+        if (i >= addresses.size()) {
+            return "Address not found";
+        }
+
+        customerRepository.save(customer);
+        return "Address updated successfully";
     }
 }
