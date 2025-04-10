e89be1aa1365def2514935cc2bf0b49c084ff734
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-10 16:18:11 +0530

    Added : added UpdateAddressCO

---
+++ src/main/java/com/Project/ecommerce/co/user/UpdateAddressCO.java
@@ -0,0 +1,14 @@
+package com.Project.ecommerce.co.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateAddressCO {
+    private String city;
+    private String state;
+    private String country;
+    private String addressLine;
+    private String zipCode;
+}
