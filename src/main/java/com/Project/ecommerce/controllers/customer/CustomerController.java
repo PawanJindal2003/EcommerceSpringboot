078e3da0f857941f535b7e4287ffecda35a73d14
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-10 16:13:26 +0530

    Updated : added update-profile, update-password, add-address, delete-address, update-address

--- src/main/java/com/Project/ecommerce/controllers/customer/CustomerController.java
+++ src/main/java/com/Project/ecommerce/controllers/customer/CustomerController.java
@@ -1,19 +1,22 @@
 package com.Project.ecommerce.controllers.customer;
 
+import com.Project.ecommerce.co.customer.UpdateProfileCO;
+import com.Project.ecommerce.co.user.UpdateAddressCO;
+import com.Project.ecommerce.co.user.UpdatePasswordCO;
 import com.Project.ecommerce.dto.customer.ViewAddressDTO;
 import com.Project.ecommerce.dto.customer.ViewProfileDTO;
+import com.Project.ecommerce.entities.address.Address;
 import com.Project.ecommerce.services.cutomer.CustomerService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Set;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -36,5 +39,40 @@ public class CustomerController {
     public ResponseEntity<List<ViewAddressDTO>> viewAddresses(HttpServletRequest request){
         List<ViewAddressDTO> addresses = customerService.getAllAddresses(request);
         return new ResponseEntity<>(addresses, HttpStatus.OK);
+    }
+
+    @PreAuthorize("getRole('CUSTOMER')")
+    @PutMapping("/update-profile")
+    public ResponseEntity<String> updateProfile(HttpServletRequest request, @Valid @RequestBody UpdateProfileCO updateProfile){
+        String responseMessage = customerService.updateProfile(request, updateProfile);
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+    }
+
+    @PreAuthorize("getRole('CUSTOMER')")
+    @PutMapping("/update-password")
+    public ResponseEntity<String> updatePassword(HttpServletRequest request, @Valid @RequestBody UpdatePasswordCO updatePasswordCO){
+        String responseMessage = customerService.updatePassword(request, updatePasswordCO);
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+    }
+
+    @PreAuthorize("getRole('CUSTOMER')")
+    @PostMapping("/add-address")
+    public ResponseEntity<String> addAddress(HttpServletRequest request, @Valid @RequestBody Address address){
+        String responseMessage = customerService.addAddress(request, address);
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+    }
+
+    @PreAuthorize("getRole('CUSTOMER')")
+    @DeleteMapping("/delete-address")
+    public ResponseEntity<String> deleteAddress(HttpServletRequest request,@Valid @RequestBody UUID id){
+        String responseMessage = customerService.deleteAddress(request, id);
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
+    }
+
+    @PreAuthorize("getRole('CUSTOMER')")
+    @PutMapping("/update-address")
+    public ResponseEntity<String> deleteAddress(HttpServletRequest request,@Valid @RequestBody UUID id, @Valid @RequestBody UpdateAddressCO updateAddressCO){
+        String responseMessage = customerService.updateAddress(request, id, updateAddressCO);
+        return new ResponseEntity<>(responseMessage, HttpStatus.OK);
     }
 }
