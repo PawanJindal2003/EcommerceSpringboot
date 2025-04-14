1c8301e3cf53528b2d7e744e7a62de0cac92d033
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-14 09:07:21 +0530

    Updated : adding roles through testcase

--- src/test/java/com/Project/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/Project/ecommerce/EcommerceApplicationTests.java
@@ -1,13 +1,25 @@
-//package com.Project.ecommerce;
+package com.Project.ecommerce;
+
+import com.Project.ecommerce.entities.user.Role;
+import com.Project.ecommerce.repositories.user.RoleRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+//    @Autowired
+//    public RoleRepository roleRepository;
+
+	@Test
+	void contextLoads() {
+	}
 //
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class EcommerceApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+//    @Test
+//    void addRoles(){
+//        roleRepository.save(new Role("ADMIN"));
+//        roleRepository.save(new Role("CUSTOMER"));
+//        roleRepository.save(new Role("SELLER"));
+//    }
+
+}
