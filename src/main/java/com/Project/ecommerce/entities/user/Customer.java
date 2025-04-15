c76ef26d0f1b842b86521b8b392029976cb7fef5
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-15 09:02:08 +0530

    Updated : removed warnings

--- src/main/java/com/Project/ecommerce/entities/user/Customer.java
+++ src/main/java/com/Project/ecommerce/entities/user/Customer.java
@@ -4,9 +4,7 @@ import com.Project.ecommerce.entities.cart.Cart;
 import com.Project.ecommerce.entities.order.OrderName;
 import com.Project.ecommerce.entities.product.ProductReview;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.criteria.Order;
 import lombok.*;
 
 import java.util.List;
