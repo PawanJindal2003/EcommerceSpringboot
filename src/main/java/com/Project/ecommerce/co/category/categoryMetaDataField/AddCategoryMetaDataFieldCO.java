512f65d91f1c065cba0b4e3ae3f8ec8eff93604a
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-16 12:01:58 +0530

    Added : CO for adding CategoryMetaDataField

---
+++ src/main/java/com/Project/ecommerce/co/category/categoryMetaDataField/AddCategoryMetaDataFieldCO.java
@@ -0,0 +1,10 @@
+package com.Project.ecommerce.co.category.categoryMetaDataField;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AddCategoryMetaDataFieldCO {
+    String value;
+}
