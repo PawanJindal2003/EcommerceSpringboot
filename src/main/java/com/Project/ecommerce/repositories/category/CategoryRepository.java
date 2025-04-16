790888747167730eab666d73dde573ba7f2e181e
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-16 12:04:17 +0530

    Added : CategoryRepository with two dynamic finder methods

---
+++ src/main/java/com/Project/ecommerce/repositories/category/CategoryRepository.java
@@ -0,0 +1,16 @@
+package com.Project.ecommerce.repositories.category;
+
+import com.Project.ecommerce.entities.category.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, String> {
+    Boolean existsByNameAndParentCategoryIdIsNull(String name);
+
+    //immediate child categories
+    Optional<List<Category>> findAllByParentCategoryId(String id);
+}
