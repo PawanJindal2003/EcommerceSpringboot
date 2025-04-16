c37964ab28f7a46f52dbf950da64de52fa5c7258
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-16 12:03:34 +0530

    Added : CategoryMetaDataFieldRepository

---
+++ src/main/java/com/Project/ecommerce/repositories/category/CategoryMetaDataFieldRepository.java
@@ -0,0 +1,15 @@
+package com.Project.ecommerce.repositories.category;
+
+import com.Project.ecommerce.entities.category.CategoryMetaDataField;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface CategoryMetaDataFieldRepository extends JpaRepository<CategoryMetaDataField, String> {
+    Optional<CategoryMetaDataField> findByName(String name);
+
+    Page<CategoryMetaDataField> findAll(Pageable pageable);
+}
