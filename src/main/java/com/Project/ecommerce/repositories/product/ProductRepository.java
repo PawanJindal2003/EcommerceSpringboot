bcac72b3d46596489def9d13a7313e2afe94f475
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-16 12:04:46 +0530

    Added : ProductRepository findByCategoryId finder methods

---
+++ src/main/java/com/Project/ecommerce/repositories/product/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.Project.ecommerce.repositories.product;
+
+import com.Project.ecommerce.entities.product.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+    Optional<Product> findByCategoryId(String id);
+}
