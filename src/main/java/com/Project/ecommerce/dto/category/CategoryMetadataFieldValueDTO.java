c619077f516c5cbf054958c1104e4e429e93921e
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-17 17:52:32 +0530

    Added : DTO for field value

---
+++ src/main/java/com/Project/ecommerce/dto/category/CategoryMetadataFieldValueDTO.java
@@ -0,0 +1,11 @@
+package com.Project.ecommerce.dto.category;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class CategoryMetadataFieldValueDTO {
+    List<String> fields;
+}
