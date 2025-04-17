5e628395d07b0104749acb095866c88ec2c32246
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-17 17:43:46 +0530

    Updated : added composite primary key

--- src/main/java/com/Project/ecommerce/entities/category/CategoryMetaDataFieldValues.java
+++ src/main/java/com/Project/ecommerce/entities/category/CategoryMetaDataFieldValues.java
@@ -9,16 +9,21 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoryMetaDataFieldValues {
+    @EmbeddedId
+    private CategoryMetaDataFieldValuesId id;
+
     private String value;
 
     //composite key
-    @Id
+//    @Id
+    @MapsId("category")
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
 
     //composite key
-    @Id
+//    @Id
+    @MapsId("categoryMetaDataField")
     @ManyToOne
     @JoinColumn(name = "category_metadata_field_id")
     private CategoryMetaDataField categoryMetaDataField;
