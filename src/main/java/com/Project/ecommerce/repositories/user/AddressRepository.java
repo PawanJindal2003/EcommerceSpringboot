3cace654f3e63e77fd0e50a9877d568288b79495
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-17 17:50:26 +0530

    Updated : added native query finder method

--- src/main/java/com/Project/ecommerce/repositories/user/AddressRepository.java
+++ src/main/java/com/Project/ecommerce/repositories/user/AddressRepository.java
@@ -3,9 +3,12 @@ package com.Project.ecommerce.repositories.user;
 import com.Project.ecommerce.entities.address.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, String> {
+    @Query(value = "SELECT * FROM address WHERE user_id = :userId AND is_deleted = false LIMIT 1", nativeQuery = true)
+    Optional<Address> findByUserId(@Param("userId") String userId);
 }
