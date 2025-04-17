25b437b012ccf20881f8407a26c0e19375c62000
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-17 17:49:48 +0530

    Updated : removed orphan true from addresses

--- src/main/java/com/Project/ecommerce/entities/user/User.java
+++ src/main/java/com/Project/ecommerce/entities/user/User.java
@@ -57,7 +57,7 @@ public class User extends Auditable {
     @ManyToOne
     private Role role;
 
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private List<Address> addresses;
 }
