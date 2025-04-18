24f2c806214ced390e8c50fa4381e98eb7d5013b
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-18 15:02:43 +0530

    Added : DTOs for view a category

---
+++ src/main/java/com/Project/ecommerce/dto/category/admin/CategoryMetaFieldsDTO.java
@@ -0,0 +1,11 @@
+package com.Project.ecommerce.dto.category.admin;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class CategoryMetaFieldsDTO {
+    List<String> fields;
+}
---
+++ src/main/java/com/Project/ecommerce/dto/category/customer/CustomerCategoryResponseDTO.java
@@ -0,0 +1,11 @@
+package com.Project.ecommerce.dto.category.customer;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerCategoryResponseDTO {
+    private String id;
+    private String name;
+}
