e05efc4eae261af5c639643146170e961b5628de
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-18 15:00:15 +0530

    Updated : added findAllByName for filtering

--- src/main/java/com/Project/ecommerce/repositories/category/CategoryRepository.java
+++ src/main/java/com/Project/ecommerce/repositories/category/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.Project.ecommerce.repositories.category;
 
 import com.Project.ecommerce.entities.category.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,4 +17,7 @@ public interface CategoryRepository extends JpaRepository<Category, String> {
 
     //immediate child categories
     Optional<List<Category>> findAllByParentCategoryId(String id);
+
+    @Query("select c from Category c where (lower(c.name) like lower(concat('%', :name, '%')))")
+    Page<Category> findAllByName(@RequestParam("name") String name, Pageable pageable);
 }
