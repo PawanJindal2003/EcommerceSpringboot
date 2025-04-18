742bcfa66ee5d4af09a55d96c512ec6fdd0b9d3e
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-18 15:00:01 +0530

    Updated : added findAllByName for filtering

--- src/main/java/com/Project/ecommerce/repositories/category/CategoryMetaDataFieldRepository.java
+++ src/main/java/com/Project/ecommerce/repositories/category/CategoryMetaDataFieldRepository.java
@@ -4,6 +4,8 @@ import com.Project.ecommerce.entities.category.CategoryMetaDataField;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
@@ -12,4 +14,7 @@ public interface CategoryMetaDataFieldRepository extends JpaRepository<CategoryM
     Optional<CategoryMetaDataField> findByName(String name);
 
     Page<CategoryMetaDataField> findAll(Pageable pageable);
+
+    @Query("select c from CategoryMetaDataField c WHERE (lower(c.name) like lower(concat('%', :name, '%')))")
+    Page<CategoryMetaDataField> findAllByName(@Param("name") String name, Pageable pageable);
 }
