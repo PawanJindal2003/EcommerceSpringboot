dbe098f140280666826788f05cc4cbaa80746a20
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-19 23:45:08 +0530

    Updated : updated isActive to false by default

--- src/main/java/com/Project/ecommerce/entities/product/Product.java
+++ src/main/java/com/Project/ecommerce/entities/product/Product.java
@@ -31,7 +31,7 @@ public class Product {
     private Boolean isCancellable;
     private Boolean isReturnable;
     private String brand;
-    private Boolean isActive;
+    private Boolean isActive = false;
     private Boolean isDeleted;
 
     @ManyToOne
