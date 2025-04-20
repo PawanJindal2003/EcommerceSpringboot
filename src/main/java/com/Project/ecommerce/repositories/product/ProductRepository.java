0d77f91cfa94b7b5b358ebf1e85ca3e2bb2e3d5a
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-20 09:19:31 +0530

    Updated : dynamic finder method using specification

--- src/main/java/com/Project/ecommerce/repositories/product/ProductRepository.java
+++ src/main/java/com/Project/ecommerce/repositories/product/ProductRepository.java
@@ -1,13 +1,18 @@
 package com.Project.ecommerce.repositories.product;
 
 import com.Project.ecommerce.entities.product.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, String>, JpaSpecificationExecutor<Product> {
     Optional<Product> findByCategoryId(String id);
     Product getNameByBrandAndSellerIdAndCategoryId(String brandName, String sellerId, String categoryId);
+//    Page<Product> findAllBySellerIdAndIsNonDeleted(Specification specification, String sellerId, Pageable pageable);
+    Page<Product> findAll(Specification<Product> specification, Pageable pageable);
 }
