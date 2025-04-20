961b4ab15df466ffa3cfde50aa56c19f4d0cf7cd
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-20 09:50:06 +0530

    Updated : added dynamic finder method by query

--- src/main/java/com/Project/ecommerce/repositories/product/ProductVariationRepository.java
+++ src/main/java/com/Project/ecommerce/repositories/product/ProductVariationRepository.java
@@ -1,9 +1,15 @@
 package com.Project.ecommerce.repositories.product;
 
 import com.Project.ecommerce.entities.product.ProductVariation;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductVariationRepository extends JpaRepository<ProductVariation, String> {
+public interface ProductVariationRepository extends JpaRepository<ProductVariation, String>, JpaSpecificationExecutor<ProductVariation> {
+    Page<ProductVariation> findAll(Pageable pageable);
+    Page<ProductVariation> findAll(Specification<ProductVariation> specification, Pageable pageable);
 }
