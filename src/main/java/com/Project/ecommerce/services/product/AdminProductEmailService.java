1e3d2dba352e4fce202363523179279661ae8ffd
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-21 15:04:25 +0530

    Added : email service regarding activation-deactivation of product

---
+++ src/main/java/com/Project/ecommerce/services/product/AdminProductEmailService.java
@@ -0,0 +1,74 @@
+package com.Project.ecommerce.services.product;
+
+import com.Project.ecommerce.entities.product.Product;
+import com.Project.ecommerce.repositories.product.ProductRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminProductEmailService {
+    private final JavaMailSender emailSender;
+
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    public AdminProductEmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    @Async
+    public void sendProductActivationEmail(String toEmail, Product product) throws MessagingException {
+
+        String subject = "Product activation status";
+        String body = "<p>Your product has been activated by admin</p>" +
+                "<h3>Product Details:</h3>" +
+                "<ul>" +
+                "<li><strong>Name:</strong> " + product.getName() + "</li>" +
+                "<li><strong>Brand:</strong> " + product.getBrand() + "</li>" +
+                "<li><strong>Category:</strong> " + product.getCategory().getName() + "</li>" +
+                "<li><strong>Description:</strong> " + product.getIsActive() + "</li>" +
+                "</ul>";
+
+        sendEmail(toEmail, subject, body);
+    }
+
+    @Async
+    public void sendProductDeactivationEmail(String toEmail, Product product) throws MessagingException {
+
+        String subject = "Account activation status";
+        String body = "<p>Your product has been deactivated by admin</p>" +
+                "<h3>Product Details:</h3>" +
+                "<ul>" +
+                "<li><strong>Name:</strong> " + product.getName() + "</li>" +
+                "<li><strong>Brand:</strong> " + product.getBrand() + "</li>" +
+                "<li><strong>Category:</strong> " + product.getCategory().getName() + "</li>" +
+                "<li><strong>Description:</strong> " + product.getIsActive() + "</li>" +
+                "</ul>";
+
+        sendEmail(toEmail, subject, body);
+    }
+
+    private void sendEmail(String toEmail, String subject, String body) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom(fromEmail);
+        helper.setTo(toEmail);
+        helper.setSubject(subject);
+        helper.setText(body, true); // Set the body as HTML
+
+        emailSender.send(message);
+    }
+
+    private String generateActivationUrl(String token) {
+        String baseUrl = "http://localhost:8080/api/admin/product-activation-status";
+        return baseUrl + "?token=" + token;
+    }
+}
