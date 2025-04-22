1b2a2c85861ebd5a3f81ffa4a38787bac9a2d1cf
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-22 17:43:56 +0530

    Updated : Corrected spelling mistake

--- src/main/java/com/Project/ecommerce/entities/product/Product.java
+++ src/main/java/com/Project/ecommerce/entities/product/Product.java
@@ -6,11 +6,9 @@ import com.Project.ecommerce.entities.user.Seller;
 import com.github.f4b6a3.uuid.UuidCreator;
 import jakarta.persistence.*;
 import lombok.*;
-aimport org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-
 import java.util.List;
-import java.util.UUID;
 
 @Entity
 @Getter
