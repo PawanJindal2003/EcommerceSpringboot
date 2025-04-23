bc25cef9764e0f3c10adc8390f314ec714b863b4
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-23 17:07:01 +0530

    Added : custom exception for image storage

---
+++ src/main/java/com/Project/ecommerce/exceptions/customExceptions/StoreImageFailureException.java
@@ -0,0 +1,7 @@
+package com.Project.ecommerce.exceptions.customExceptions;
+
+public class StoreImageFailureException extends RuntimeException{
+    public StoreImageFailureException(String message){
+        super(message);
+    }
+}
