99b48f3f47718d2980c255f621b7bf86a046a808
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-24 12:32:12 +0530

    Added : custom exception, if seller sends more than one address

---
+++ src/main/java/com/Project/ecommerce/exceptions/customExceptions/ExcessAddressesException.java
@@ -0,0 +1,7 @@
+package com.Project.ecommerce.exceptions.customExceptions;
+
+public class ExcessAddressesException extends RuntimeException{
+    public ExcessAddressesException(String message){
+        super(message);
+    }
+}
