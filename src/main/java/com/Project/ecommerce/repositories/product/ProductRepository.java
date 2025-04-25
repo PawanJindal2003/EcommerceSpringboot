93c3fb75fa4ff6e7c3d08e6887fad49688906a5b
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-25 17:21:26 +0530

    Updated : corrected findByBrandAndSellerIdAndCategoryId finder method

--- src/main/java/com/Project/ecommerce/repositories/product/ProductRepository.java
+++ src/main/java/com/Project/ecommerce/repositories/product/ProductRepository.java
@@ -7,12 +7,15 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String>, JpaSpecificationExecutor<Product> {
     Optional<Product> findByCategoryId(String id);
-    Product getNameByBrandAndSellerIdAndCategoryId(String brandName, String sellerId, String categoryId);
-//    Page<Product> findAllBySellerIdAndIsNonDeleted(Specification specification, String sellerId, Pageable pageable);
+
+    List<Product> findByBrandAndSellerIdAndCategoryId(String brandName, String sellerId, String categoryId);
+
+    //    Page<Product> findAllBySellerIdAndIsNonDeleted(Specification specification, String sellerId, Pageable pageable);
     Page<Product> findAll(Specification<Product> specification, Pageable pageable);
 }
